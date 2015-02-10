0c9fa3436119bede54c994ff089f291cb91a4662
moliri <lmaliakal@u.northwestern.edu>
2015-02-10 00:18:07 -0600

    drawing route on the map

--- CrowdCheer/DetailViewController.m
+++ CrowdCheer/DetailViewController.m
@@ -8,6 +8,9 @@
 
 #import "DetailViewController.h"
 #import <MapKit/MapKit.h>
+#import "MathController.h"
+#import "Run.h"
+#import "Location.h"
 
 @interface DetailViewController () <MKMapViewDelegate>
 
@@ -33,6 +36,17 @@
 
 - (void)configureView
 {
+    self.distanceLabel.text = [MathController stringifyDistance:self.run.distance.floatValue];
+    
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    [formatter setDateStyle:NSDateFormatterMediumStyle];
+    self.dateLabel.text = [formatter stringFromDate:self.run.timestamp];
+    
+    self.timeLabel.text = [NSString stringWithFormat:@"Time: %@",  [MathController stringifySecondCount:self.run.duration.intValue usingLongFormat:YES]];
+    
+    self.paceLabel.text = [NSString stringWithFormat:@"Pace: %@",  [MathController stringifyAvgPaceFromDist:self.run.distance.floatValue overTime:self.run.duration.intValue]];
+    
+    [self loadMap];
 }
 
 - (void)viewDidLoad
@@ -40,5 +54,93 @@
     [super viewDidLoad];
     [self configureView];
 }
+
+- (MKCoordinateRegion)mapRegion
+{
+    MKCoordinateRegion region;
+    Location *initialLoc = self.run.locations.firstObject;
+    
+    float minLat = initialLoc.latitude.floatValue;
+    float minLng = initialLoc.longitude.floatValue;
+    float maxLat = initialLoc.latitude.floatValue;
+    float maxLng = initialLoc.longitude.floatValue;
+    
+    for (Location *location in self.run.locations) {
+        if (location.latitude.floatValue < minLat) {
+            minLat = location.latitude.floatValue;
+        }
+        if (location.longitude.floatValue < minLng) {
+            minLng = location.longitude.floatValue;
+        }
+        if (location.latitude.floatValue > maxLat) {
+            maxLat = location.latitude.floatValue;
+        }
+        if (location.longitude.floatValue > maxLng) {
+            maxLng = location.longitude.floatValue;
+        }
+    }
+    
+    region.center.latitude = (minLat + maxLat) / 2.0f;
+    region.center.longitude = (minLng + maxLng) / 2.0f;
+    
+    region.span.latitudeDelta = (maxLat - minLat) * 1.1f; // 10% padding
+    region.span.longitudeDelta = (maxLng - minLng) * 1.1f; // 10% padding
+    
+    return region;
+}
+
+- (MKOverlayRenderer *)mapView:(MKMapView *)mapView rendererForOverlay:(id < MKOverlay >)overlay
+{
+    if ([overlay isKindOfClass:[MKPolyline class]]) {
+        MKPolyline *polyLine = (MKPolyline *)overlay;
+        MKPolylineRenderer *aRenderer = [[MKPolylineRenderer alloc] initWithPolyline:polyLine];
+        aRenderer.strokeColor = [UIColor blackColor];
+        aRenderer.lineWidth = 3;
+        return aRenderer;
+    }
+    
+    return nil;
+}
+
+- (MKPolyline *)polyLine {
+    
+    CLLocationCoordinate2D coords[self.run.locations.count];
+    
+    for (int i = 0; i < self.run.locations.count; i++) {
+        Location *location = [self.run.locations objectAtIndex:i];
+        coords[i] = CLLocationCoordinate2DMake(location.latitude.doubleValue, location.longitude.doubleValue);
+    }
+    
+    return [MKPolyline polylineWithCoordinates:coords count:self.run.locations.count];
+}
+
+- (void)loadMap
+{
+    if (self.run.locations.count > 0) {
+        
+        self.mapView.hidden = NO;
+        
+        // set the map bounds
+        [self.mapView setRegion:[self mapRegion]];
+        
+        // make the line(s!) on the map
+        [self.mapView addOverlay:[self polyLine]];
+        
+    } else {
+        
+        // no locations were found!
+        self.mapView.hidden = YES;
+        
+        UIAlertView *alertView = [[UIAlertView alloc]
+                                  initWithTitle:@"Error"
+                                  message:@"Sorry, this run has no locations saved."
+                                  delegate:nil
+                                  cancelButtonTitle:@"OK"
+                                  otherButtonTitles:nil];
+        [alertView show];
+    }
+}
+
+
 
 @end
