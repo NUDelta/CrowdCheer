be83a90752d57e691748a19c80ef6fcd77dfadf4
christinakim <christinahkim1@gmail.com>
2015-03-03 15:51:07 -0600

    added relationship view

---
+++ RelationshipViewController.h
@@ -0,0 +1,13 @@
+//
+//  RelationshipViewController.h
+//  CrowdCheer
+//
+//  Created by Christina Kim on 3/3/15.
+//  Copyright (c) 2015 Delta Lab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RelationshipViewController : UIViewController
+
+@end
---
+++ RelationshipViewController.m
@@ -0,0 +1,16 @@
+//
+//  RelationshipViewController.m
+//  CrowdCheer
+//
+//  Created by Christina Kim on 3/3/15.
+//  Copyright (c) 2015 Delta Lab. All rights reserved.
+//
+
+#import "RelationshipViewController.h"
+
+@implementation RelationshipViewController
+
+- (void)viewDidLoad {
+    NSLog(@"relationship here!");
+}
+@end
