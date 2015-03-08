257616313a094f35cac2edf8f4e8a98285ddda7c
christinakim <christinahkim1@gmail.com>
2015-03-08 17:45:15 -0500

    added assigned runner's goal pace label for relationship view

--- RelationshipViewController.m
+++ RelationshipViewController.m
@@ -7,10 +7,38 @@
 //
 
 #import "RelationshipViewController.h"
+#import "NewRunViewController.h"
+#import "DetailViewController.h"
+#import "Run.h"
+#import <CoreLocation/CoreLocation.h>
+#import "MathController.h"
+#import "Location.h"
+#import <Parse/Parse.h>
+#import <Parse/PFGeoPoint.h>
+
+@interface RelationshipViewController () <UIActionSheetDelegate, CLLocationManagerDelegate>
+
+
+@property int seconds;
+@property float distance;
+@property NSString *pace;
+@property (nonatomic, strong) CLLocationManager *locationManager;
+@property (nonatomic, strong) NSMutableArray *locations;
+@property (nonatomic, strong) NSTimer *timer;
+
+@property (nonatomic, weak) IBOutlet UILabel *goalPace;
+
+@end
+
 
 @implementation RelationshipViewController
 
 - (void)viewDidLoad {
-    NSLog(@"relationship here!");
+    [super viewDidLoad];
 }
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
 @end
