e708b536af8d4d9c7fd923ab28167fabcdff0d91
Scott Allen Cambo <scottcambo2019@u.northwestern.edu>
2015-03-11 12:31:51 -0500

    added RelationshipViewController.*

---
+++ CrowdCheer/RelationshipViewController.h
@@ -0,0 +1,13 @@
+//
+//  RelationshipViewController.h
+//  CrowdCheer
+//
+//  Created by Scott Cambo on 3/11/15.
+//  Copyright (c) 2015 Delta Lab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RelationshipViewController : UIViewController
+
+@end
---
+++ CrowdCheer/RelationshipViewController.m
@@ -0,0 +1,37 @@
+//
+//  RelationshipViewController.m
+//  CrowdCheer
+//
+//  Created by Scott Cambo on 3/11/15.
+//  Copyright (c) 2015 Delta Lab. All rights reserved.
+//
+
+#import "RelationshipViewController.h"
+
+@interface RelationshipViewController ()
+
+@end
+
+@implementation RelationshipViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
