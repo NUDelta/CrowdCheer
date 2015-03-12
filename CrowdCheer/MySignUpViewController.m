d1483812139749c3fbb0642e58eb54b197ab6d4c
christinakim <christinahkim1@gmail.com>
2015-03-11 20:52:14 -0500

    fixed sign up button

--- CrowdCheer/MySignUpViewController.m
+++ CrowdCheer/MySignUpViewController.m
@@ -62,7 +62,7 @@
     CGRect fieldFrame = self.signUpView.usernameField.frame;
     
     [self.signUpView.dismissButton setFrame:CGRectMake(10.0f, 10.0f, 87.5f, 45.5f)];
-    [self.signUpView.logo setFrame:CGRectMake(66.5f, 70.0f, 187.0f, 58.5f)];
+   // [self.signUpView.logo setFrame:CGRectMake(66.5f, 70.0f, 187.0f, 58.5f)];
     [self.signUpView.signUpButton setFrame:CGRectMake(35.0f, 385.0f, 250.0f, 40.0f)];
     //    [self.fieldsBackground setFrame:CGRectMake(35.0f, fieldFrame.origin.y + yOffset, 250.0f, 174.0f)];
     
@@ -96,8 +96,8 @@
     //
     //    [self.signUpView.dismissButton setFrame:CGRectMake(10.0f, 10.0f, 87.5f, 45.5f)];
     //    [self.signUpView.logo setFrame:CGRectMake(66.5f, 70.0f, 187.0f, 58.5f)];
-    //    [self.signUpView.signUpButton setFrame:CGRectMake(fieldFrame.origin.x, 480.0f, 250.0f, 40.0f)];
-    //    [self.signUpView.signUpButton setTitle:@"SIGN UP" forState:UIControlStateNormal];
+       [self.signUpView.signUpButton setFrame:CGRectMake(fieldFrame.origin.x, 480.0f, 250.0f, 40.0f)];
+       [self.signUpView.signUpButton setTitle:@"SIGN UP" forState:UIControlStateNormal];
     //
     //    [self.signUpView.usernameField setFrame:CGRectMake(fieldFrame.origin.x + 5.0f,
     //                                                       fieldFrame.origin.y + yOffset,
