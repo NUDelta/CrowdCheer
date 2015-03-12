e9735e545100b9a777e2a3f3143158abb443e766
moliri <lmaliakal@u.northwestern.edu>
2015-03-12 10:11:47 -0500

    fixed passing of data via notification

--- CrowdCheer/RelationshipViewController.m
+++ CrowdCheer/RelationshipViewController.m
@@ -16,6 +16,7 @@
 #import <Parse/Parse.h>
 #import <Parse/PFGeoPoint.h>
 
+
 @interface RelationshipViewController () <UIActionSheetDelegate, CLLocationManagerDelegate>
 
 @property (nonatomic, strong) Run *run;
@@ -39,22 +40,33 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     //Once we have the Runner's account as user, we can use this code to pull data for the motivator:
-//    PFFile *userImageFile = user[@"profilePic"];
-//    [userImageFile getDataInBackgroundWithBlock:^(NSData *imageData, NSError *error) {
-//        if (!error) {
-//            UIImage *profilePic = [UIImage imageWithData:imageData];
-//            self.imageView.image = profilePic;
-//        }
-//    }];
-//    
-//    NSString *name = user[@"name"];
-//    NSString *bibNumber = user[@"bibNumber"];
-//    NSString *commonality = user[@"display commonality here"];
-//    NSLog(name);
-//    
-//    _nameLabel.text = name;
-//    _bibLabel.text = bibNumber;
-//    _commonalityLabel.text = commonality;
+    NSString *userObjectID = [self.userInfo objectForKey:@"objectId"];
+    PFQuery *query = [PFUser query];
+    PFUser *user = (PFUser *)[query getObjectWithId:userObjectID];
+    
+    if(!user) {
+        NSLog(@"ERROR: No user object passed.");
+    }
+    else {
+        PFFile *userImageFile = user[@"profilePic"];
+        [userImageFile getDataInBackgroundWithBlock:^(NSData *imageData, NSError *error) {
+            if (!error) {
+                UIImage *profilePic = [UIImage imageWithData:imageData];
+                self.imageView.image = profilePic;
+            }
+        }];
+        
+        NSString *name = user[@"name"];
+        NSString *bibNumber = user[@"bibNumber"];
+        NSString *commonality = user[@"display commonality here"];
+        NSLog(name);
+        
+        _nameLabel.text = name;
+        _bibLabel.text = bibNumber;
+        _commonalityLabel.text = commonality;
+    }
+    
+    
 }
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
