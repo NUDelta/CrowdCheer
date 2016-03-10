baf7fd3704aa67a1c035a5d7a3d9e99bbad22f8f
moliri <lmaliakal@u.northwestern.edu>
2016-03-10 16:11:03 -0600

    modifying shell script, storyboard changes

--- Delta_Enterprise_Export.sh
+++ Delta_Enterprise_Export.sh
@@ -3,7 +3,7 @@
 
 xcodebuild clean -project CrowdCheer -configuration Release -alltargets
 xcodebuild -workspace CrowdCheer.xcworkspace clean archive -scheme CrowdCheer -archivePath CrowdCheer.xcarchive
-xcodebuild -exportArchive -archivePath CrowdCheer.xcarchive -exportPath CrowdCheer2 -exportFormat ipa -exportProvisioningProfile "Delta Lab"
+xcodebuild -exportArchive -archivePath CrowdCheer.xcarchive -exportPath CrowdCheer2 -exportFormat ipa -exportProvisioningProfile "Delta"
 mv -i CrowdCheer2.ipa  CrowdCheer.ipa
 
 # xcodebuild 
