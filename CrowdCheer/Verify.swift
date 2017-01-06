2693b70de158c901b56df57d29f27ef3e123f292
moliri <lmaliakal@u.northwestern.edu>
2017-01-05 19:28:29 -0600

    cleaning up verify protocols

--- CrowdCheer/Verify.swift
+++ CrowdCheer/Verify.swift
@@ -15,25 +15,17 @@ protocol Deliver: Any {
     var user: PFUser {get}
     var locationMgr: CLLocationManager {get}
     var location: CLLocation {get set}
-    var recordingSession: AVAudioSession! {get set}
-    var audioRecorder: AVAudioRecorder! {get set}
     
     func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation])
     func spectatorDidCheer(runner: PFUser, didCheer: Bool)
-    func startRecordingSpectatorAudio(runnerName: String)
-    func stopRecordingSpectatorAudio()
 }
 
 protocol Receive: Any {
     var user: PFUser {get}
     var locationMgr: CLLocationManager {get}
     var location: CLLocation {get set}
-    var recordingSession: AVAudioSession! {get set}
-    var audioRecorder: AVAudioRecorder! {get set}
     
     func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation])
-    func startRecordingRunnerAudio()
-    func stopRecordingRunnerAudio()
 }
 
 protocol React: Any {
@@ -50,8 +42,6 @@ class VerifiedDelivery: NSObject, Deliver, CLLocationManagerDelegate {
     var user: PFUser
     var locationMgr: CLLocationManager
     var location: CLLocation
-    var recordingSession: AVAudioSession!
-    var audioRecorder: AVAudioRecorder!
     
     override init(){
         user = PFUser.currentUser()!
@@ -100,57 +90,12 @@ class VerifiedDelivery: NSObject, Deliver, CLLocationManagerDelegate {
         }
     }
     
-    func startRecordingSpectatorAudio(runnerName: String) {
-        
-        //initialize recording session
-        recordingSession = AVAudioSession.sharedInstance()
-        
-        do {
-            try recordingSession.setCategory(AVAudioSessionCategoryPlayAndRecord)
-            try recordingSession.setActive(true)
-            recordingSession.requestRecordPermission({ (allowed) in
-                if allowed {
-                    print("recording permission granted")
-                } else {
-                    print("ERROR: permission denied for audio")
-                }
-            })
-        }
-        
-        catch {
-            print("ERROR: error initializing audio")
-        }
-        
-        //start recording
-        let audioFilenameString = user.username! + "_" + runnerName + ".m4a"
-        let audioFilename = getDocumentsDirectory().URLByAppendingPathComponent(audioFilenameString)
-        let settings = [
-            AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
-            AVSampleRateKey: 12000,
-            AVNumberOfChannelsKey: 1,
-            AVEncoderAudioQualityKey: AVAudioQuality.High.rawValue
-        ]
-        
-        do {
-            audioRecorder = try AVAudioRecorder(URL: audioFilename!, settings: settings)
-//            audioRecorder.delegate = self
-            audioRecorder.record()
-            
-        }
-        catch {
-            print("ERROR: error recording audio")
-        }
-    }
-    
     func getDocumentsDirectory() -> NSURL {
         let paths = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)
         let documentsDirectory = paths[0]
         return documentsDirectory
     }
-    
-    func stopRecordingSpectatorAudio() {
-        audioRecorder.stop()
-    }
+
 }
 
 class VerifiedReceival: NSObject, Receive, CLLocationManagerDelegate {
@@ -183,56 +128,11 @@ class VerifiedReceival: NSObject, Receive, CLLocationManagerDelegate {
     func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         
     }
-    func startRecordingRunnerAudio() {
-        
-        //initialize recording session
-        recordingSession = AVAudioSession.sharedInstance()
-        
-        do {
-            try recordingSession.setCategory(AVAudioSessionCategoryPlayAndRecord)
-            try recordingSession.setActive(true)
-            recordingSession.requestRecordPermission({ (allowed) in
-                if allowed {
-                    print("recording permission granted")
-                } else {
-                    print("ERROR: permission denied for audio")
-                }
-            })
-        }
-            
-        catch {
-            print("ERROR: error initializing audio")
-        }
-        
-        //start recording
-        let audioFilenameString = user.username! + ".m4a"
-        let audioFilename = getDocumentsDirectory().URLByAppendingPathComponent(audioFilenameString)
-        let settings = [
-            AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
-            AVSampleRateKey: 12000,
-            AVNumberOfChannelsKey: 1,
-            AVEncoderAudioQualityKey: AVAudioQuality.High.rawValue
-        ]
-        
-        do {
-            audioRecorder = try AVAudioRecorder(URL: audioFilename!, settings: settings)
-            //            audioRecorder.delegate = self
-            audioRecorder.record()
-            
-        }
-        catch {
-            print("ERROR: error recording audio")
-        }
-    }
     
     func getDocumentsDirectory() -> NSURL {
         let paths = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)
         let documentsDirectory = paths[0]
         return documentsDirectory
-    }
-    
-    func stopRecordingRunnerAudio() {
-        audioRecorder.stop()
     }
 }
 
