a3d28e587f701a49b67ae10c8cb072da7b7c15f2
moliri <lmaliakal@u.northwestern.edu>
2017-05-12 23:09:18 -0500

    setting start line for Lakefill

--- CrowdCheer/Views/RunViewController.swift
+++ CrowdCheer/Views/RunViewController.swift
@@ -45,7 +45,7 @@ class RunViewController: UIViewController, MKMapViewDelegate {
 //        let startLine = CLLocationCoordinate2DMake(42.057102, -87.676943) //ford
 //        let startLine = CLLocationCoordinate2DMake(42.058175, -87.683502) //noyes el
 //        let startLine = CLLocationCoordinate2DMake(42.051169, -87.677232) //arch
-        let startLine = CLLocationCoordinate2DMake(41.947186, -87.658938) //race
+        let startLine = CLLocationCoordinate2DMake(42.050829, -87.671498) //race
         let startRegion = runnerMonitor.createStartRegion(startLine)
         runnerMonitor.startMonitoringRegion(startRegion)
         
