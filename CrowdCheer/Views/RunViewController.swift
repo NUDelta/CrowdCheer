b7d2cf434fb1a73282308236d14970f2c89a7805
moliri <lmaliakal@u.northwestern.edu>
2018-08-10 17:31:55 -0500

    set race start geofence for tracking, tested and working in sim

--- CrowdCheer/Views/RunViewController.swift
+++ CrowdCheer/Views/RunViewController.swift
@@ -96,7 +96,7 @@ class RunViewController: UIViewController, MKMapViewDelegate {
 //        let startLine = CLLocationCoordinate2DMake(42.057102, -87.676943) //ford
 //        let startLine = CLLocationCoordinate2DMake(42.058175, -87.683502) //noyes el
 //        let startLine = CLLocationCoordinate2DMake(42.051169, -87.677232) //arch
-        let startLine = CLLocationCoordinate2DMake(41.880782, -87.619641) //race
+        let startLine = CLLocationCoordinate2DMake(41.859896, -87.613899) //race
         let startRegion = runnerMonitor.createStartRegion(startLine)
         runnerMonitor.startMonitoringRegion(startRegion)
         
